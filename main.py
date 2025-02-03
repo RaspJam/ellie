56bf0fff793dff80cbea207cc6485292725d3556
ZackeryRSmith <zackery.smith82307@gmail.com>
2025-02-03 17:28:03 -0500

    command api testing

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from src.wakeword import listen_for_wakeword
 import src.mic as mic
 import numpy as np
 import time
-import sounddevice as sd
+import spacy
+from src.coin import Coin, BANK
 
-model_size = "base"
+model_size = "small"
 
 log.info("Initalize WhisperModel")
 whisperModel = WhisperModel(
     model_size, device="cpu", compute_type="int8", cpu_threads=4
 )
+
+log.info("Load COINS model")
+nlp = spacy.load('COINS')
 
 log.info("Yerp")
 while True:
@@ -19,9 +23,18 @@ while True:
         log.info("Wakeword Detected!")
 
         segments, _ = whisperModel.transcribe(mic.get_chunk())
+        seg = list(segments)[0]
 
-        for segment in segments:
-            print(f"[{segment.start:.2f}s - {segment.end:.2f}s]: {segment.text}")
+        log.debug(seg.text)
+        doc = nlp(seg.text)
+
+        coin = Coin()
+        for ent in doc.ents:
+            setattr(coin, ent.label_[0].lower(), ent.text.lower())
+
+        log.debug(coin)
+
+        BANK.data[coin.c][coin.o]()
 
         log.fatal("cuz why not")
         exit(0)
